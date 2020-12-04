9a32fd13670a53227e3bfc79b002f97bf892f30d
ajotamm99 <72545812+ajotamm99@users.noreply.github.com>
2020-12-04 16:03:19 +0100

    fix

--- 2Drogue/Assets/Scripts/Enemy.cs
+++ 2Drogue/Assets/Scripts/Enemy.cs
@@ -11,12 +11,14 @@ public class Enemy : MovingObject {
 	private bool skipMove;
 	public AudioClip enemyAttack1;
 	public AudioClip enemyAttack2;
+	private SpriteRenderer spriteRenderer;
 	bool move = false;
 
 
 	protected override void Start () {
 		GameManager.instance.AddEnemyToList(this);
 		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 		target = GameObject.FindGameObjectWithTag("Player").transform;
 		base.Start();
 	}
@@ -33,28 +35,71 @@ public class Enemy : MovingObject {
 	public void MoveEnemy() {
 		int xDir = 0;
 		int yDir = 0;
+		bool horizontal = false;
+		bool vertical = false;
 
-		if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+		if (Mathf.Abs(target.position.x - transform.position.x) > float.Epsilon)
 		{
+			horizontal = true;
+		}
+		if (Mathf.Abs(target.position.y - transform.position.y) > float.Epsilon)
+		{
+			vertical = true;
+		}
+
+		if(vertical) {
+			xDir = 0;
 			yDir = target.position.y > transform.position.y ? 1 : -1;
-			if (CanMove(xDir, yDir) == false)
-			{
-				yDir = 0;
-				xDir = target.position.x > transform.position.x ? 1 : -1;
+			if(CanMove(xDir,yDir)) {
+				AttemptMove<Player>(xDir, yDir);
+				return;
 			}
 		}
 
-		else
+		if (horizontal)
 		{
 			xDir = target.position.x > transform.position.x ? 1 : -1;
-			if (CanMove(xDir, yDir) == false)
+			yDir = 0;
+
+			// Flips sprite following horizontal direction
+			if(xDir < 0)
 			{
-				xDir = 0;
-				yDir = target.position.y > transform.position.y ? 1 : -1;
+				spriteRenderer.flipX = false;
+			} else
+			{
+				spriteRenderer.flipX = true;
 			}
+
+
+			if (CanMove(xDir, yDir))
+			{
+				AttemptMove<Player>(xDir, yDir);
+				return;
+			}
+
 		}
 
-		AttemptMove<Player>(xDir, yDir);
+//		if (Mathf.Abs(target.position.x - transform.position.x) > float.Epsilon)
+//		{
+//			yDir = target.position.y > transform.position.y ? 1 : -1;
+//			if (CanMove(xDir, yDir) == false)
+//			{
+//				yDir = 0;
+//				xDir = target.position.x > transform.position.x ? 1 : -1;
+//			}
+//		}
+//
+//		else
+//		{
+//			xDir = target.position.x > transform.position.x ? 1 : -1;
+//			if (CanMove(xDir, yDir) == false)
+//			{
+//				xDir = 0;
+//				yDir = target.position.y > transform.position.y ? 1 : -1;
+//			}
+//		}
+//
+//		AttemptMove<Player>(xDir, yDir);
 
 	}
 
